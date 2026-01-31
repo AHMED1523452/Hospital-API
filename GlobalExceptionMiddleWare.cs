b8880a2c5aca19888a9be745bdef9b80aa9b4867
Ahmed Mohamed <mandelo632@gmail.com>
2026-01-31 19:52:15 +0200

    middleware

---
+++ GlobalExceptionMiddleWare.cs
@@ -0,0 +1,28 @@
+﻿namespace HealthSync.MiddleWare
+{
+    public class GlobalExceptionMiddleWare
+    {
+        public GlobalExceptionMiddleWare(RequestDelegate _next)
+        {
+            Next = _next;
+        }
+
+        public RequestDelegate Next { get; }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                //. make the request can arrive to the server or the controller or the action
+                await Next(context);
+            }
+            catch(Exception ex)
+            {
+                context.Response.StatusCode = 500;
+                await context.Response.WriteAsJsonAsync(new
+                {
+                    message = ex.Message
+                });
+            }
+        }
+    }
+}
